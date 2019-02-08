495eb3ee20cfa45c0bfc4b0a2ae116ef420968a1
Andreas <andreas@makeable.dk>
2019-02-08 09:57:10 +0100

    Revised vision setup to only need a single function for setting up the vision detection.

--- firebasevisionlib/src/main/java/dk/makeable/firebasevisionlib/FirebaseVisionActivity.kt
+++ firebasevisionlib/src/main/java/dk/makeable/firebasevisionlib/FirebaseVisionActivity.kt
@@ -4,51 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class FirebaseVisionActivity: AppCompatActivity() {
 
-    private lateinit var cameraSource: CameraSource
-    private lateinit var cameraSourcePreview: CameraSourcePreview
-    private lateinit var graphicOverlay: GraphicOverlay
-
-    /**
-     * Return the RecognitionProcessor associated with this activity.
-     */
-    abstract fun getRecognitionProcessor(): RecognitionProcessor
+    private var firebaseVision: FirebaseVisionSetup? = null
 
     /**
      * When this function is called, a call to getRecognitionProcessor will be called immediately after. Will set up the camera.
      * Be sure to follow this up with a call to startCameraSource for the preview to begin updating.
      */
-    fun setupVisionDetection(graphicOverlay: GraphicOverlay, cameraSourcePreview: CameraSourcePreview) {
-        createCameraSource(graphicOverlay, cameraSourcePreview)
+    fun setupVisionDetection(graphicOverlay: GraphicOverlay,
+                             cameraSourcePreview: CameraSourcePreview,
+                             recognitionProcessor: RecognitionProcessor) {
+
+        this.attachFirebaseVision(FirebaseVisionSetup(this, graphicOverlay, cameraSourcePreview, recognitionProcessor))
+//        createCameraSource(graphicOverlay, cameraSourcePreview)
     }
 
-    fun startCameraSource() {
-        if (!this::cameraSourcePreview.isInitialized) {
-            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
-        }
-        cameraSourcePreview.start(cameraSource, graphicOverlay)
+    private fun attachFirebaseVision(visionSetup: FirebaseVisionSetup) {
+        this.firebaseVision = visionSetup
     }
 
-    fun stopCameraSource() {
-        if (!this::cameraSourcePreview.isInitialized) {
-            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
-        }
-        cameraSourcePreview.stop()
-    }
-
-    fun releaseCameraSource() {
-        if (!this::cameraSourcePreview.isInitialized) {
-            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
-        }
-        cameraSource.release()
-    }
-
-    private fun createCameraSource(graphicOverlay: GraphicOverlay, cameraSourcePreview: CameraSourcePreview) {
-        this.graphicOverlay = graphicOverlay
-        this.cameraSourcePreview = cameraSourcePreview
-        cameraSource = CameraSource(this, graphicOverlay)
-
-        // Set the processor
-        cameraSource.setMachineLearningFrameProcessor(getRecognitionProcessor())
-    }
+//    fun startCameraSource() {
+//        if (!this::cameraSourcePreview.isInitialized) {
+//            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
+//        }
+//        cameraSourcePreview.start(cameraSource, graphicOverlay)
+//    }
+//
+//    fun stopCameraSource() {
+//        if (!this::cameraSourcePreview.isInitialized) {
+//            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
+//        }
+//        cameraSourcePreview.stop()
+//    }
+//
+//    fun releaseCameraSource() {
+//        if (!this::cameraSourcePreview.isInitialized) {
+//            throw UninitializedPropertyAccessException("CameraSource is not initialized, be sure to call setupVisionDetection before starting/stopping the CameraSource.")
+//        }
+//        cameraSource.release()
+//    }
+//
+//    private fun createCameraSource(graphicOverlay: GraphicOverlay, cameraSourcePreview: CameraSourcePreview) {
+//        this.graphicOverlay = graphicOverlay
+//        this.cameraSourcePreview = cameraSourcePreview
+//        cameraSource = CameraSource(this, graphicOverlay)
+//
+//        // Set the processor
+//        cameraSource.setMachineLearningFrameProcessor(getRecognitionProcessor())
+//    }
 
 }
---
+++ firebasevisionlib/src/main/java/dk/makeable/firebasevisionlib/FirebaseVisionSetup.kt
@@ -0,0 +1,50 @@
+package dk.makeable.firebasevisionlib
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+/**
+ * This class uses a reference to an activity, and a reference to a CameraSourcePreview/GraphicOverlay to control and release the camera when needed based on the activities lifecycle.
+ */
+class FirebaseVisionSetup(private val activity: AppCompatActivity,
+                          private val graphicOverlay: GraphicOverlay,
+                          private val cameraSourcePreview: CameraSourcePreview,
+                          private val recognitionProcessor: RecognitionProcessor): LifecycleObserver {
+
+    private val cameraSource: CameraSource = CameraSource(activity, graphicOverlay)
+
+    private var started: Boolean = true
+
+    init {
+        cameraSource.setMachineLearningFrameProcessor(recognitionProcessor)
+
+        // Register this object to the activity lifecycle
+        activity.lifecycle.addObserver(this)
+
+        start() // Start if not already.
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private fun start() {
+        if (activity.lifecycle.currentState.isAtLeast(Lifecycle.State.CREATED) && !started) {
+            started = true
+            cameraSourcePreview.start(cameraSource, graphicOverlay)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private fun stop() {
+        if (started) {
+            started = false
+            cameraSourcePreview.stop()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun release() {
+        cameraSource.release()
+    }
+
+}
