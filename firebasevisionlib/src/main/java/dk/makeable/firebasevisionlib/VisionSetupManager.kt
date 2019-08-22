294a4eec8e501f79d2fbefdb85ce2633f2be5844
Andreas Dybdahl <andreasdybdahl@Andreass-MacBook-Pro.local>
2019-08-22 11:31:34 +0200

    Revised VisionSetupManager, as setups were added multiple times, fixed by revising setups type to a Set.

--- firebasevisionlib/src/main/java/dk/makeable/firebasevisionlib/VisionSetupManager.kt
+++ firebasevisionlib/src/main/java/dk/makeable/firebasevisionlib/VisionSetupManager.kt
@@ -2,7 +2,7 @@ package dk.makeable.firebasevisionlib
 
 class VisionSetupManager private constructor() {
 
-    private val setups: MutableList<FirebaseVisionSetup<*>> = mutableListOf()
+    private val setups: MutableSet<FirebaseVisionSetup<*>> = hashSetOf() // Set prevents duplicates
 
     private object Holder {
         val INSTANCE = VisionSetupManager()
